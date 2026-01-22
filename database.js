55284f151e46d2c5ba506f9b2085ccd7aac74d91
EDUARDO <gatitopbeiop@gmail.com>
2026-01-22 02:06:13 -0500

    Arreglando conexion SSL y puerto

--- database.js
+++ database.js
@@ -1,21 +1,24 @@
 const mysql = require('mysql2');
 require('dotenv').config();
+
 // CREAMOS LA CONEXION
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    
-})
+    port: process.env.DB_PORT, // <--- Agregamos el puerto para TiDB (4000)
+    ssl: {
+        rejectUnauthorized: false // <--- ¡ESTO ES LA CLAVE! Permite la conexión segura
+    }
+});
 
-db.connect(err =>{
-    if (err ) {
-     console.log('ERROR AL CONECTAR LA BASE DE DATOS: ', err);
-     return;
+db.connect(err => {
+    if (err) {
+        console.log('ERROR AL CONECTAR LA BASE DE DATOS: ', err);
+        return;
     }
     console.log('CONEXIÓN EXITOSA A LA BASE DE DATOS MYSQL')
-})
+});
 
-
-module.exports= db; //EXPORTAR EL OBJETO CONEXIÓN
+module.exports = db;
